e7714f5b176fb86a5acf12088317d0f7c8120fdb
DESKTOP-S0OAMUC\northka.chen <northka.chen@gmail.com>
2016-11-10 16:56:46 +0800

    bug修复， 不去寻找host

--- test/profile.js
+++ test/profile.js
@@ -4,27 +4,16 @@
 module.exports = {
 	status: 'online',
 	urls   : {
-		online: 'http://baidu.lecai.com'
+		online: '10.10.62.103'
 	},
 	method : 'GET',
-	engine   : 'http',
+	engine : 'http',
+    port   : 9299,
 	interfaces : [
 		{
 			id   : 'getItem',
-			url  : '10.10.62.103',
-			port : 9299,
-			urls : {
-				online : '10.10.62.103'
-			},
 			http : {
 				path : '/admin/v1/activitys-guess/history'
-			}
-		},
-		{
-			id   : 'getItem1',
-			url  : '/lottery/draw/ajax_get_detail.php',
-			urls : {
-				online : '10.10.62.103'
 			}
 		}
 	]
